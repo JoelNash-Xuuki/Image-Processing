c26dd22696af880dcd16dfe20cf9cdd958d2c9a1
JoelNash-Xuuki <joelnash@xuuki.xyz>
2022-11-16 07:48:49 +0000

    update

--- src/image-processor.sh
+++ src/image-processor.sh
@@ -1,39 +1,53 @@
-#PROJ="$2"
-WAV="${HOME}/audio/$PROJ.wav"
-TEMPWOA="${HOME}/motion-picture/$PROJ-temp-wo-audio.mp4"
-WAVMIX="${HOME}/audio/$PROJ-mix.wav"
-MP4="${HOME}/motion-picture/$PROJ.mp4"
-TITLE="${HOME}/image/title.png" 
-TESTING="${HOME}/image/test.png" 
+PROJ="03102022180354"
+WAV="$HOME/audio/$PROJ.wav"
+TEMPWOA="$HOME/motion-picture/$PROJ-temp-wo-audio.mp4"
+WAVMIX="$HOME/audio/$PROJ-mix.wav"
+MP4R="$HOME/motion-picture/$PROJ-tt.mp4"
+TITLE="$HOME/image/title.png" 
+TESTING="$HOME/image/test.png" 
 
 getProjName(){
   echo $PROJ;
 }
+
 ipsg(){
-ffmpeg -ss 00:00:00 -i $2 -t 22.784 -filter_complex "[0:a]showspectrum=s=854x480:mode=combined:slide=scroll:saturation=0.2:scale=log,format=yuv420p[v]" -map "[v]" -map 0:a -b:v 700k -b:a 360k $3
+ffmpeg -ss 00:00:00 -i $1 -t 34.28544 -filter_complex "[0:a]showspectrum=s=854x480:mode=combined:slide=scroll:saturation=0.2:scale=log,format=yuv420p[v]" -map "[v]" -map 0:a -b:v 700k -b:a 360k $2
+#ffmpeg -ss 00:00:00 -i /home/joel/motion-picture/03102022180354.mp4 -t 22.784 -filter_complex "[0:a]showspectrum=s=854x480:mode=combined:slide=scroll:saturation=0.2:scale=log,format=yuv420p[v]" -map "[v]" -map 0:a -b:v 700k -b:a 360k 03102022180354-testvid.mp4
 }
 
 overlayVideo(){
   #echo "TEST"
-  overlayv $MP4 $MP4R $TEMPWOA $TEMPWA
+  overlay $MP4 $MP4R $TEMPWOA $TEMPWA
   #cp ${HOME}/motion-picture/${TEMPWA} $MP4
   #rm ${HOME}/motion-picture/${TEMPWA}
 }
 
-#convert -background transparent -fill lightblue \
-#          -font ~/.fonts/xuukitype1.ttf -pointsize 144 label:X \
-#          ${HOME}/image/title.png
+createTitles(){
+convert -background transparent -fill lightblue \
+          -font ~/.fonts/xuukitype1.ttf -pointsize 144 label:X \
+          ${HOME}/images/X.png
+
+convert -background transparent -fill lightpink \
+          -font ~/.fonts/xuukitype1.ttf -pointsize 144 label:VYVYAN \
+          ${HOME}/images/VYVYAN.png
+}
+
+createWaterMark(){
+  ffmpeg -i $1 -vf "movie=$2 [watermark]; [in][watermark] overlay [out]" output01.mp4 
+}
 
 ipaddimage(){
   local image st end
-  image=$3 
-  st=$4
-  nd=$5
-  overlay $1 $image $2  $st $nd
-  cp $2 $1
-  rm $2 
+  image=$1 
+  st=$2
+  nd=$3
+  overlay $4 $image $5 $st $nd
+  #cp $2 $1
+  #rm $2 
 }
+
 iprot(){
   ffmpeg -i $1 -vf "transpose=1" $2
 }
+
 "$@"
