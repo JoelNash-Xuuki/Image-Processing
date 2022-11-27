ce7f5c7ff2a3028bb6436ed292ad5960160e0be4
JoelNash-Xuuki <joelnash@xuuki.xyz>
2022-11-27 21:38:30 +0000

    added funcntion to image-processor.sh

--- src/image-processor.sh
+++ src/image-processor.sh
@@ -11,13 +11,13 @@ getProjName(){
 }
 
 ipsg(){
-ffmpeg -ss 00:00:00 -i $1 -t 34.28544 -filter_complex "[0:a]showspectrum=s=854x480:mode=combined:slide=scroll:saturation=0.2:scale=log,format=yuv420p[v]" -map "[v]" -map 0:a -b:v 700k -b:a 360k $2
+ffmpeg -ss 00:00:00 -i $1 -t $3 -filter_complex "[0:a]showspectrum=s=854x480:mode=combined:slide=scroll:saturation=0.2:scale=log,format=yuv420p[v]" -map "[v]" -map 0:a -b:v 700k -b:a 360k $2
 #ffmpeg -ss 00:00:00 -i /home/joel/motion-picture/03102022180354.mp4 -t 22.784 -filter_complex "[0:a]showspectrum=s=854x480:mode=combined:slide=scroll:saturation=0.2:scale=log,format=yuv420p[v]" -map "[v]" -map 0:a -b:v 700k -b:a 360k 03102022180354-testvid.mp4
 }
 
 overlayVideo(){
   #echo "TEST"
-  overlay $MP4 $MP4R $TEMPWOA $TEMPWA
+#  overlay $MP4 $MP4R $TEMPWOA $TEMPWA
   #cp ${HOME}/motion-picture/${TEMPWA} $MP4
   #rm ${HOME}/motion-picture/${TEMPWA}
 }
@@ -44,10 +44,6 @@ ipaddimage(){
   overlay $4 $image $5 $st $nd
   #cp $2 $1
   #rm $2 
-}
-
-iprot(){
-  ffmpeg -i $1 -vf "transpose=1" $2
 }
 
 "$@"
